04df0ef2c380dbfa56c8e72203e9855d9797b79c
Kyuuhachi <caagr98@gmail.com>
2024-01-15 14:40:17 +0100

    Make a better cli frontend

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
 #!/usr/bin/env node
-import * as process from "process";
-import * as fs from "fs";
+import * as process from "node:process";
+import * as fsp from "node:fs/promises";
+
+import { program } from 'commander';
 import * as prettier from "prettier";
 import { cleanString } from "./clean.js";
 
-const input = fs.readFileSync(process.stdin.fd, 'utf-8');
-const output = cleanString(input);
-prettier.format(output, {
-	parser: "babel",
-	printWidth: 98,
-	useTabs: true,
-}).then(output => {
-	fs.writeFileSync(process.stdout.fd, output);
-})
+program.option("-n, --no-prettier").option("--prettier");
+program.parse();
+const opts = program.opts();
+
+if(program.args.length) {
+	const results = []
+	for(const path of program.args) {
+		const input = await fsp.readFile(path);
+		const output = await run(input);
+		console.log(path);
+		results.push({
+			path,
+			output,
+		});
+	}
+	for(const { path, output } of results) {
+		await fsp.writeFile(path, output);
+	}
+} else {
+	const inbuf = [];
+	for await(const chunk of process.stdin) inbuf.push(chunk);
+	const input = Buffer.concat(inbuf);
+	const output = await run(input);
+	await process.stdout.write(output);
+}
+
+async function run(buf) {
+	let text = buf.toString('utf8')
+	text = cleanString(text);
+	if(opts.prettier) {
+		text = await prettier.format(text, {
+			parser: "babel",
+			printWidth: 98,
+			useTabs: true,
+		})
+	}
+	return text;
+}
